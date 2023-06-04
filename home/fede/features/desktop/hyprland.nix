ada46d95cb338a35a8cdf53c6d539f7072a8d5db
MrPinko <77388947+MrPinko@users.noreply.github.com>
2023-06-04 19:07:13 +0200

    test

--- home/fede/features/desktop/hyprland.nix
+++ home/fede/features/desktop/hyprland.nix
@@ -1,9 +1,16 @@
 { inputs, lib, config, pkgs, ... }: {
   imports = [
+    ../common
+    ../common/wayland-wm
     inputs.hyprland.homeManagerModules.default
   ];
 
   programs = {
+    fish.loginShellInit = ''
+      if test (tty) = "/dev/tty1"
+        exec Hyprland &> /dev/null
+      end
+    '';
     zsh.loginExtra = ''
       if [ "$(tty)" = "/dev/tty1" ]; then
         exec Hyprland &> /dev/null
@@ -16,13 +23,29 @@
     '';
   };
 
+
+  home.packages = with pkgs; [
+    inputs.hyprwm-contrib.packages.${system}.grimblast
+    swaybg
+    swayidle
+    # TODO
+    # inputs.hyprland.packages.${system}.xdg-desktop-portal-hyprland
+  ];
+
   programs.waybar.package = pkgs.waybar.overrideAttrs (oa: {
     mesonFlags = (oa.mesonFlags or  [ ]) ++ [ "-Dexperimental=true" ];
   });
 
   wayland.windowManager.hyprland = {
     enable = true;
-    nvidiaPatches = true;
     package = inputs.hyprland.packages.${pkgs.system}.default;
+    extraConfig =
+      (import ./monitors.nix {
+        inherit lib;
+        inherit (config) monitors;
+      }) +
+      (import ./config.nix {
+        inherit (config) home colorscheme wallpaper;
+      });
   };
 }
--- hosts/common/optional/greetd.nix
+++ hosts/common/optional/greetd.nix
@@ -1,12 +1,27 @@
-{...}: {
+{ pkgs, ... }:
+let
+  user = "fede";
+  greetd = "${pkgs.greetd.greetd}/bin/greetd";
+  gtkgreet = "${pkgs.greetd.gtkgreet}/bin/gtkgreet";
+
+  sway-kiosk = command: "${pkgs.sway}/bin/sway --config ${pkgs.writeText "kiosk.config" ''
+    output * bg #000000 solid_color
+    exec dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK
+    exec "${command}; ${pkgs.sway}/bin/swaymsg exit"
+  ''}";
+in
+{
   services.greetd = {
     enable = true;
-    settings = rec {
-      initial_session = {
-        command = "Hyprland";
-        user = "fede";
+    settings = {
+      default_session = {
+        command = sway-kiosk "${gtkgreet} -l -c '$SHELL -l'";
+        inherit user;
       };
-      default_session = initial_session;
+      initial_session = {
+        command = "$SHELL -l";
+        inherit user;
+      };
     };
   };
 }
